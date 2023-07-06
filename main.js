7d9cfb97ead8db41f23235552c009b4cdf0de077
Bonello <geno.bonello@gmail.com>
2023-07-06 12:31:41 -0700

    now shows all drinks that match criteria

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ let ingredientsArr = []
 const ing1 = document.getElementById("ingredient1")
 const ing2 = document.getElementById("ingredient2")
 const ing3 = document.getElementById("ingredient3")
+
 const cocktails = {
     "apiKey": '9973533',
     fetchCocktails: function(ingredient1, ingredient2, ingredient3){
@@ -13,31 +14,24 @@ const cocktails = {
         + this.apiKey +
         "/filter.php?i=" + ingredient1 + "," + ingredient2 + "," + ingredient3)
         .then((response) => response.json())
-        .then((data) => this.cocktailLookup(data.drinks[0].idDrink, data.drinks[1].idDrink))
+        // .then((data) =>  console.log(data))
+        .then((data) =>  this.cocktailLookup(data.drinks))
         .catch((error) => {
             console.log(error)
           })
     },
-    cocktailLookup: function(id1, id2){
+    cocktailLookup: function(drinksArr){
+        for (let i = 0; i < drinksArr.length; i++){
         fetch("https:/www.thecocktaildb.com/api/json/v2/"
         + this.apiKey + 
         "/lookup.php?i="
-        + id1)
+        + drinksArr[i].idDrink)
         .then((response) => response.json())
         .then((data) => console.log(data))
         .catch((error) => {
             console.log(error)
           })
-
-        fetch("https:/www.thecocktaildb.com/api/json/v2/"
-        + this.apiKey + 
-        "/lookup.php?i=" 
-        + id2)
-        .then((response) => response.json())
-        .then((data) => console.log(data))
-        .catch((error) => {
-            console.log(error)
-          })
+        }   
     },
     ingredientLookup: function(){
         fetch("https:/www.thecocktaildb.com/api/json/v2/"
@@ -51,11 +45,9 @@ const cocktails = {
     },
     pushIngredients: function(data){
         for (let i = 0; i<data.drinks.length; i++){
-            ingredientsArr.push(data.drinks[i].strIngredient1)
-            
+            ingredientsArr.push(data.drinks[i].strIngredient1)          
         }
     },
-
 }
 
 cocktails.ingredientLookup()
